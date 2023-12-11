3542d2e29333850368ebd299998b3240f56111c4
Matias Camiletti <matias.camiletti@gmail.com>
2023-12-11 16:06:51 -0300

    Create structure.

---
+++ src/Providers/TotsEmailProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tots\Email\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TotsEmailProvider extends ServiceProvider
+{
+    /**
+     * Boot the blog services for the application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
